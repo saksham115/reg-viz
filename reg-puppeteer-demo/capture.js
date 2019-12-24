d380ff0c5f501bb1cef108f59be07e04e5ddbe31
sakham115 <saksham@browserstack.com>
2019-12-24 17:38:18 +0530

    test

---
+++ reg-puppeteer-demo/capture.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer');
+const mkdirp = require('mkdirp');
+
+async function main() {
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  let page = await browser.newPage();
+
+  await page.setViewport({ width: 640, height: 680 });
+  await page.goto(`file://${__dirname}/index.html`);
+  await new Promise(res => setTimeout(() => res(), 300));
+  await page.screenshot({ path: 'screenshot/index.png' });
+
+  await page.close();
+  await browser.close();
+}
+
+mkdirp.sync('screenshot');
+main();
